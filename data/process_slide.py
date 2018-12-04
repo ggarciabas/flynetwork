0a8e0ca0136a102e862ee0d1321779aa2cab85e1
Giovanna <ggarciabas@gmail.com>
2018-12-04 08:50:57 -0200

    Atualizando scripts de slide

--- data/process_slide.py
+++ data/process_slide.py
@@ -14,39 +14,48 @@ if sys.argv[1] == "False":
 
 scenario = sys.argv[2]
 
-title = sys.argv[4]
-folder = sys.argv[5]
-
 main_path = "./scratch/flynetwork/data/output/"+scenario+"/"
 if teste :
     main_path = "./output/"+scenario+"/"
 
-list_folder = []
+if not os.path.exists(main_path+'../slide'):
+    os.mkdir(main_path+'../slide')
 
-if int(sys.argv[3]) == -1: # folder number
-    for folder_name in glob.glob(main_path+'*/'):
-        list_folder.append(int(os.path.dirname(folder_name).split('/')[-1]))
-else:
-    list_folder.append(int(sys.argv[3]))
+custos = ["custo_1", "custo_2", "custo_3", "custo_4"]
+for custo in custos:
+    title = ""
+    if custo == "custo_1":
+        title = "Custo 1"
+    elif custo == "custo_2":
+        title = "Custo 2"
+    elif custo == "custo_3":
+        title = "Custo 3"
+    elif custo == "custo_4":
+        title = "Custo 4"
+    folder = custo
+    list_folder = []
 
-if teste:
-    print (list_folder)
+    if int(sys.argv[3]) == -1: # folder number
+        for folder_name in glob.glob(main_path+custo+'/*/'):
+            list_folder.append(int(os.path.dirname(folder_name).split('/')[-1]))
+    else:
+        list_folder.append(int(sys.argv[3]))
 
-list_folder = np.array(list_folder)
-list_folder.sort()
+    if teste:
+        print (list_folder)
 
-# criar pasta TODO
-if not os.path.exists(main_path+'../'+folder):
-    os.mkdir(main_path+'../'+folder)
+    list_folder = np.array(list_folder)
+    list_folder.sort()
 
-for time in list_folder:
-    uav_loc_slide.slide (str(time), main_path, teste, title, folder)
-    intermediario.slide (str(time), main_path, teste, title, folder)
+    # criar pasta TODO
+    if not os.path.exists(main_path+'../slide/'+folder):
+        os.mkdir(main_path+'../slide/'+folder)
 
-file = open(main_path+'..'+'/slide_'+folder+'.tex', 'w')
-for time in list_folder:
-    if len(glob.glob(main_path+'../'+folder+'/slide_'+str(time)+'.tex'))>0:
-        file.write("\\input{"+folder+'/'+os.path.basename(glob.glob(main_path+'../'+folder+'/slide_'+str(time)+'.tex')[0])+"}\n")
-    if len(glob.glob(main_path+'../'+folder+'/slide_intermediario_'+str(time)+'.tex'))>0:
-        file.write("\\input{"+folder+'/'+os.path.basename(glob.glob(main_path+'../'+folder+'/slide_intermediario_'+str(time)+'.tex')[0])+"}\n")
-file.close()
+    for time in list_folder:
+        uav_loc_slide.slide (str(time), main_path+custo+'/', teste, title, folder)
+
+    file = open(main_path+'..'+'/slide/slide_'+folder+'.tex', 'w')
+    for time in list_folder:
+        if len(glob.glob(main_path+'../slide/'+folder+'/slide_'+str(time)+'.tex'))>0:
+            file.write("\\input{"+folder+'/'+os.path.basename(glob.glob(main_path+'../slide/'+folder+'/slide_'+str(time)+'.tex')[0])+"}\n")
+    file.close()
---
+++ data/run_slide_processing.sh
@@ -0,0 +1,4 @@
+
+# $1- scenario name
+
+python process_slide.py True $1 -1
