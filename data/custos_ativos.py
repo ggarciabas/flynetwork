ad7f05b1d46716fc061e4db7ed7c56afc96c73c0
Giovanna <ggarciabas@gmail.com>
2019-05-13 15:04:44 -0300

    Separando repositorios

--- data/custos_ativos.py
+++
@@ -1,2 +0,0 @@
-c_name = {"custo_1":"Custo 1", "custo_2":"Custo 2", "custo_3":"Custo 3", "custo_4":"Custo 4"}
-data_c = {"Custo 1":[], "Custo 2":[], "Custo 3":[], "Custo 4":[]}
--- data/run_exaustive.sh
+++
@@ -1,5 +0,0 @@
-# $1 - True or False (debug)
-# $2 - Scenario (name)
-# $3 - Etapa nao usado
-
-python exhaustive_prop.py $1 $2 #$3
